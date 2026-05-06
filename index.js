52fc2f5e3923636b0958fc5a3bf1ff6fe4fc6659
Karan-Developer-69 <homepc8890@gmail.com>
2026-05-06 19:06:41 +0530

    stable

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-throw new Error("App crashed");
+const express = require('express');
+const app = express();
+const port = 3000;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
